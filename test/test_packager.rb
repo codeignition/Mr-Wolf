9d207686638b84b47d2d2a7fa73907f32189360e
vipulfdgod <vipulsharma2190@gmail.com>
2013-10-29 13:17:36 +0530

    [vipul] clean up

--- test/test_packager.rb
+++ test/test_packager.rb
@@ -8,7 +8,7 @@ class TC_mrwolf < Test::Unit::TestCase
     project="project"
     version="0.1"
     prefix="/opt"
-   `mrwolf package -t #{path} -n #{project} -v #{version} -p #{prefix}`
+   `mrwolf package -t #{path} -n #{project} -v #{version} -p #{prefix};ls`
 
     assert_same(File.exist?("#{project}"+"-"+"#{version}"+"-1"".noarch.rpm"),true)
     `rm -f #{project}-#{version}-1.noarch.rpm`
